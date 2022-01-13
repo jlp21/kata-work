afe44e8fc85df9184b62f918f3af37604183c4e8
jlp21 <portelli21@gmail.com>
2022-01-12 20:16:26 -0500

    added tweet component

--- labs.js
+++ labs.js
@@ -46,3 +46,43 @@ function App() {
 }
 
 export default App;
+
+//Tweet component
+
+import ProfileImage from "./ProfileImage";
+
+function Tweet(props) {
+  console.log("props",props)
+  return (
+    <div className="tweet">
+    <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <span className="user">
+            <span className="name">{props.tweet.user.name}</span>
+            <span className="handle">{props.tweet.user.handle}</span>
+          </span>
+
+          <span className="timestamp">{props.tweet.timestamp}</span>
+        </div>
+
+        <p className="message">
+          {props.tweet.message}
+        </p>
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+        </div>
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
