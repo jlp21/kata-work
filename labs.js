39f66b15afd8589a8481b247459a183764ad18ac
jlp21 <portelli21@gmail.com>
2022-01-29 22:32:17 -0500

    added component to tweet lab

--- labs.js
+++ labs.js
@@ -98,3 +98,18 @@ function ProfileImage(props) {
 }
 
 export default ProfileImage;
+
+
+function User(props) {
+
+  return (
+
+      <span className="user">
+          <span className="name"> {props.userData.name} </span>
+          <span className="handle"> {props.userData.handle} </span>
+      </span>
+
+  );
+}
+
+export default User;
