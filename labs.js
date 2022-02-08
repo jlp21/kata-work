8ad7bb01ae8fa16013b18ab389d11a15b7ed2ed5
jlp21 <portelli21@gmail.com>
2022-02-07 22:48:06 -0500

    lab done

--- labs.js
+++ labs.js
@@ -136,3 +136,19 @@ function Message(props) {
 }
 
 export default Message;
+
+function Actions() {
+
+  return (
+
+      <div className="actions">
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+      </div>
+
+  );
+}
+
+export default Actions;
